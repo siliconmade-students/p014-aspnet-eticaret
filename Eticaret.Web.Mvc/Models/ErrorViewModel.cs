5e39b5d843f1f8e80ef8fc48bf7f98db0d210ef6
Cem Gündüzoğlu <cmg.web@gmail.com>
2023-04-01 13:05:11 +0300

    MVC projesi eklendi

---
+++ Eticaret.Web.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eticaret.Web.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
