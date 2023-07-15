1483da7a1623201051fd06d2d484301c391fd4e1
Cem Gündüzoğlu <cmg.web@gmail.com>
2023-07-15 13:19:34 +0300

    Orders admin sayfası eklendi

---
+++ Eticaret.Web.Mvc/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,177 @@
+﻿using Eticaret.Data;
+using Eticaret.Data.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eticaret.Web.Mvc.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly EticaretDbContext _context;
+
+        public OrdersController(EticaretDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Orders
+        public async Task<IActionResult> Index()
+        {
+            var eticaretDbContext = _context.Orders.Include(o => o.User).Include(o => o.UsersAddress);
+            return View(await eticaretDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.UsersAddress)
+                .Include(o => o.OrderProducts).ThenInclude(o => o.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "EmailAddress");
+            ViewData["UsersAddressId"] = new SelectList(_context.UserAddresses, "Id", "Address");
+            return View();
+        }
+
+        // POST: Admin/Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,OrderNumber,UsersAddressId,TotalPrice,OrderStatus,CreatedAt,Id")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.CreatedAt = DateTime.Now;
+
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "EmailAddress", order.UserId);
+            ViewData["UsersAddressId"] = new SelectList(_context.UserAddresses, "Id", "Address", order.UsersAddressId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "EmailAddress", order.UserId);
+            ViewData["UsersAddressId"] = new SelectList(_context.UserAddresses, "Id", "Address", order.UsersAddressId);
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,OrderNumber,UsersAddressId,TotalPrice,OrderStatus,Id")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "EmailAddress", order.UserId);
+            ViewData["UsersAddressId"] = new SelectList(_context.UserAddresses, "Id", "Address", order.UsersAddressId);
+            return View(order);
+        }
+
+        // GET: Admin/Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.User)
+                .Include(o => o.UsersAddress)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Admin/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Orders == null)
+            {
+                return Problem("Entity set 'EticaretDbContext.Orders'  is null.");
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return (_context.Orders?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
